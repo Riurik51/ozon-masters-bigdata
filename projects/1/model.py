02b234aa2c8e788bbd943f481abcb105a0301bd6
Riurik51 <riurik5@mail.ru>
2019-10-25 14:02:56 +0300

    1 dz commit

---
+++ projects/1/model.py
@@ -0,0 +1,55 @@
+
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.ensemble import RandomForestClassifier
+
+#
+# Dataset fields
+#
+numeric_features = ["if"+str(i) for i in range(1,14)]
+categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
+fields = ["id", "label"] + numeric_features + categorical_features
+
+#
+#Feature selection
+#
+
+column = [6, 9, 13, 16, 17, 19]
+categorical_features = ["cf"+str(i) for i in column]
+numeric_features = ["if"+str(i) for i in range(1,12)]
+mask = [False] * 42
+for i in range(len(column)):
+    column[i] += 14
+for i in column:
+    mask[i] = True
+for i in range(2,13):
+    mask[i] = True
+#
+# Model pipeline
+#
+
+# We create the preprocessing pipelines for both numeric and categorical data.
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', StandardScaler())
+])
+
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_features)
+    ]
+)
+
+# Now we have a full prediction pipeline.
+model = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('decisionTree', RandomForestClassifier(n_estimators=3,criterion='entropy', max_depth=512, n_jobs=-1))
+])
---
+++ projects/1/predict.py
@@ -0,0 +1,44 @@
+#!/opt/conda/envs/dsenv/bin/python
+
+import sys, os
+import logging
+from joblib import load
+import pandas as pd
+
+sys.path.append('.')
+
+numeric_features = ["if"+str(i) for i in range(1,14)]
+categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
+fields = ["id"] + numeric_features + categorical_features
+
+mask = [False] * 41
+column = [6, 9, 13, 16, 17, 19]
+for i in range(len(column)):
+    column[i] += 13
+for i in column:
+    mask[i] = True
+for i in range(1,12):
+    mask[i] = True
+
+#
+# Init the logger
+#
+logging.basicConfig(level=logging.DEBUG)
+logging.info("CURRENT_DIR {}".format(os.getcwd()))
+logging.info("SCRIPT CALLED AS {}".format(sys.argv[0]))
+logging.info("ARGS {}".format(sys.argv[1:]))
+
+#load the model
+model = load("1.joblib")
+
+
+#read and infere
+read_opts=dict(
+        sep='\t', names=fields, index_col=False, header=None,
+        iterator=True, chunksize=100
+)
+
+for df in pd.read_csv(sys.stdin, **read_opts):
+    pred = model.predict(df.loc[:, mask])
+    out = zip(df.id, pred)
+    print("\n".join(["{0}\t{1}".format(*i) for i in out]))
